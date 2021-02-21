8792165e6c35a38083c6f3821fb23cf5e39efa32
GCN-Sittha <sittha@goconnext.com>
2021-02-21 21:42:25 +0700

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1_ArrangList
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        ArrayList arrayList;
+        public MainWindow()
+        {
+            InitializeComponent();
+            arrayList = new ArrayList();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (txtBox.Text == " ")
+            {
+                MessageBox.Show("Please input data here");
+            }
+            else
+            {
+                arrayList.Add(txtBox.Text);
+                MessageBox.Show(txtBox.Text);
+                txtBox.Text = " ";
+            }
+
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (txtBox.Text == " ")
+            {
+                MessageBox.Show("Please input you data here");
+            } else
+            {
+                arrayList.Remove(txtBox.Text);
+                MessageBox.Show(txtBox.Text);
+                txtBox.Text = " ";
+            }
+           
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            foreach (string data in arrayList)
+            {
+                MessageBox.Show(data.ToString());
+            }
+
+        }
+    }
+}
